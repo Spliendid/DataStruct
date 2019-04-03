51959d88258b023aa6d823800d91923a7b2f68d1
ArthurYu <598228123@qq.com>
2019-04-03 10:54:14 +0800

    BST 增 查

--- BST/BST.cs
+++ BST/BST.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+/*
+ * 二分搜索树性质
+ * 1.二分搜索每个节点的值不同
+ * 2.每个节点的值大于左子树所有节点的值，小于右子树所有节点的值
+ * 3.每个子树也是二分搜索树
+ */
 namespace BST
 {
     /// <summary>
@@ -51,7 +56,76 @@ namespace BST
         {
             return size == 0;
         }
+
+        #region Add
+
+
+        public void Add(T t)
+        {
+            root = Add(root, t);
+        }
+
+        private Node Add(Node node,T t)
+        {
+            //增加自身
+            if (node == null)
+            {
+                return new Node(t);
+            }
+           
+            //添加子节点
+            if (t.Compare(t,node.t)>0)
+            {
+                node.right = Add(node.right,t);
+            }
+            else if (t.Compare(t, node.t) < 0)
+            {
+                node.left = Add(node.left,t);
+            }
+
+            return node;
+        }
+        #endregion
+
+        #region Find
+
+        public bool Contains(T t)
+        {
+            return Contains(root,t);
+        }
+
+        private bool Contains(Node node, T t)
+        {
+
+            if (node == null)
+            {
+                return false;
+            }
+
+            if (node.t.Equals(t))
+            {
+                return true;
+            }
+            else if (t.Compare(t,node.t)>0)
+            {
+                return Contains(node.right, t);
+            }
+            else
+            {
+                return Contains(node.left,t);
+            }
+        }
+
+
+        #endregion
+
+        //public override string ToString()
+        //{
+        //    StringBuilder sb = new StringBuilder();
+
+        //}
     }
 
-    
+
+
 }
